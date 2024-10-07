aaba4e875efa9ed51cc130d1c65fa594ffd16653
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2024-10-07 16:57:38 +0530

    Added all files

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import mainRouter from './routes/mainRouter.js';
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const PORT:string|number=process.env.PORT||3000
+
+app.use('/api/v1',mainRouter);
+
+app.listen(PORT,()=>{
+  console.log(`server start at PORT ${PORT}`)
+})
